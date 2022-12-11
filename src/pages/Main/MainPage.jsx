6ba83a421a03f5924f89524d44ead484cda881bb
khabLexander <mantillagka@hotmail.com>
2022-12-10 22:42:31 -0500

    footer

--- src/pages/Main/MainPage.jsx
+++ src/pages/Main/MainPage.jsx
@@ -2,18 +2,22 @@ import { FantasyLayout } from "../../ui/FantasyLayout";
 import { TeamsList } from "./components/TeamsList";
 import { LeaguesList } from "./components/LeaguesList";
 import { PlayerList } from "./components/PlayerList";
+import { Footer } from "../../ui/components/Footer";
 
 export const MainPage = () => {
   return (
-    <FantasyLayout>
-      <div className="main-content">
-        <PlayerList />
-        <br />
-        <TeamsList />
-        <br />
-        <LeaguesList />
-        <br />
-      </div>
-    </FantasyLayout>
+    <>
+      <FantasyLayout>
+        <div className="main-content">
+          <PlayerList />
+          <br />
+          <TeamsList />
+          <br />
+          <LeaguesList />
+          <br />
+        </div>
+      </FantasyLayout>
+      <Footer />
+    </>
   );
 };
--- src/ui/FantasyLayout.jsx
+++ src/ui/FantasyLayout.jsx
@@ -1,19 +1,12 @@
 import React from "react";
-import { Footer } from "./components/Footer";
 import { NavBar } from "./components/NavBar";
 
-const containerStyle = {
-  width: "90%",
-  paddingBottom: '180px',
-  margin: "30px auto",
-};
 
 export const FantasyLayout = ({ children }) => {
   return (
     <div>
       <NavBar />
-      <div style={containerStyle}>{children}</div>
-      <Footer />
+      <div >{children}</div>
     </div>
   );
 };
